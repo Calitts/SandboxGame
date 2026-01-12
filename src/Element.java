9745f0b0ec7e5dc3577f990e990eefc0ab7dae76
MaryMaruh <valentineeloisa05@gmail.com>
2026-01-11 21:50:02 -0300

    Update Element.java

--- src/Element.java
+++ src/Element.java
@@ -10,25 +10,27 @@ abstract class Elemento {
 
     public String nome;
     public int cor;
-    public int peso;          // 0 = sem gravidade | 10 = muito pesado
+    public int peso;   // 0 = sem gravidade | 10 = muito pesado
+    public boolean solido;
     public boolean liquido;
     public boolean gasoso;
     public boolean inflamavel;
 
-    public Elemento(String nome, int cor, int peso,
+    public Elemento(String nome, int cor, int peso,boolean solido,
                     boolean liquido, boolean gasoso,
                     boolean inflamavel) {
 
         this.nome = nome;
         this.cor = cor;
         this.peso = peso;
+        this.solido = solido;
         this.liquido = liquido;
         this.gasoso = gasoso;
         this.inflamavel = inflamavel;
     }
 
     public boolean sofreGravidade() {
-        return peso > 0 && !gasoso;
+        return (solido || liquido) && peso > 0;
     }
 }
 
@@ -39,86 +41,105 @@ abstract class Elemento {
 // Líquido  que apaga fogo
 class Agua extends Elemento {
     public Agua() {
-        super("Água", 0x0000FF, 5, true, false, false);
+        super("Água", 0x0000FF, 5, false, true, false, false);
     }
 }
 
 // Elemento granuloso que cai
 class Areia extends Elemento {
     public Areia() {
-        super("Areia", 0xFFFF00, 6, false, false, false);
+        super("Areia", 0xFFFF00, 6, true, false, false, false);
     }
 }
 
 // Queima elementos inflamáveis
 class Fogo extends Elemento {
     public Fogo() {
-        super("Fogo", 0xFF4500, 0, false, false, false);
+        super("Fogo", 0xFF4500, 0, false, false, false, false);
     }
 }
 
 // Gás sem cor
 class Ar extends Elemento {
     public Ar() {
-        super("Ar", 0x000000, 0, false, true, false);
+        super("Ar", 0x000000, 0, false, false, true, false);
     }
 }
 
 // Gás leve 
 class Vapor extends Elemento {
     public Vapor() {
-        super("Vapor", 0xCCCCCC, 1, false, true, false);
+        super("Vapor", 0xCCCCCC, 1, false, false, true, false);
     }
 }
 
 //  sólido e inflamável
 class Madeira extends Elemento {
     public Madeira() {
-        super("Madeira", 0x8B4513, 7, false, false, true);
+        super("Madeira", 0x8B4513, 7,true,  false, false, true);
     }
 }
 
 // Resultado da combustão
 class Fumaca extends Elemento {
     public Fumaca() {
-        super("Fumaça", 0x777777, 1, false, true, false);
+        super("Fumaça", 0x777777, 1, false, false, true, false);
     }
 }
 
 // Mistura de água e areia
 class Lama extends Elemento {
     public Lama() {
-        super("Lama", 0x6B4423, 6, true, false, false);
+        super("Lama", 0x6B4423, 6, true, true, false, false);
     }
 }
 
 // Sólido pesado e resistente
 class Pedra extends Elemento {
     public Pedra() {
-        super("Pedra", 0x555555, 9, false, false, false);
+        super("Pedra", 0x555555, 9, true, false, false, false);
     }
 }
 
 // Rocha derretida
 class Lava extends Elemento {
     public Lava() {
-        super("Lava", 0xFF3300, 8, true, false, false);
+        super("Lava", 0xFF3300, 8, true, true, false, false);
     }
 }
 
 // Material formado da areia super-aquecida
 class Vidro extends Elemento {
     public Vidro() {
-        super("Vidro", 0x99FFFF, 8, false, false, false);
+        super("Vidro", 0x99FFFF, 8, true, false, false, false);
     }
 }
 
 // Resultado sólido da queima
 class Cinzas extends Elemento {
     public Cinzas() {
-        super("Cinzas", 0xAAAAAA, 2, false, false, false);
+        super("Cinzas", 0xAAAAAA, 2, true, false, false, false);
     }
+  // Líquido inflamável
+class Oleo extends Elemento {
+    public Oleo() { super("Óleo", 0xFFD700, 5, false,  true, false, true); }
 }
+
+// Elemento elétrico
+class Eletricidade extends Elemento {
+    public Eletricidade() { super("Eletricidade", 0xFFFFAA, 0, false, false, true, false); }
+}
+
+// Elemento congelado
+class Neve extends Elemento {
+    public Neve() { super("Neve", 0xFFFFFF, 2, true, false, false, false); }
+}
+
+// Metal sólido pesado
+class Metal extends Elemento {
+    public Metal() { super("Metal", 0x808080, 10, true, false, false, false); }
+}
+
 
 
    //LOGICA DE REAÇÕES
@@ -187,6 +208,41 @@ class GerenciadorReacoes {
                 Lava.class, Areia.class,
                 new Vidro(), new Vidro()
         ));
+
+       // Fogo + Fumaça → Cinzas
+    reacoes.add(new Reacao(
+            Fogo.class, Fumaca.class,
+            new Cinzas(), new Ar()
+    ));
+      // Fogo + Oleo → Propagacao
+    reacoes.add(new Reacao(
+            Fogo.class, Oleo.class,
+            new Fogo(), new Fogo()
+    ));
+
+      // Eletricidade + Agua → Propagacao
+    reacoes.add(new Reacao(
+            Eletricidade.class, Agua.class,
+            new Eletricidade(), new Agua()
+    ));
+
+      // Eletricidade + Oleo → curto circuito
+    reacoes.add(new Reacao(
+            Eletricidade.class, Oleo.class,
+            new Fogo(), new Oleo()
+    ));
+
+      // Neve + Lava → Agua + Vapor
+    reacoes.add(new Reacao(
+            Neve.class, Lava.class,
+            new Agua(), new Vapor()
+    ));
+
+      // Fogo + Neve → Agua
+    reacoes.add(new Reacao(
+            Fogo.class, Neve.class,
+            new Agua(), new Vapor()
+    ));
     }
 
     // Método que o JOGO vai chamar
